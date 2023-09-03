c0ba79c253ec40f44c6c78ff0fd9f6b020dfbf27
asukaze55 <mail@asukaze.net>
2023-09-03 12:42:56 +0900

    Rename cassava_ui.js to cassava_menu.js.

--- pack/build.cmd
+++ pack/build.cmd
@@ -4,8 +4,8 @@ CALL perl toModule.pl < ../cassava_grid_data.js > src/cassava_grid_data.js
 CALL perl toModule.pl < ../cassava_undo_grid.js > src/cassava_undo_grid.js
 CALL perl toModule.pl < ../cassava_dom.js > src/cassava_dom.js
 CALL perl toModule.pl < ../cassava_grid.js > src/cassava_grid.js
-CALL perl toModule.pl < ../cassava_ui.js > src/cassava_ui.js
-CALL tsc src/cassava_ui.js --target es2021 --checkJs --noEmit
+CALL perl toModule.pl < ../cassava_menu.js > src/cassava_menu.js
+CALL tsc src/cassava_menu.js --target es2021 --checkJs --noEmit
 CALL webpack
 
 SET FILENAME=cassava_min_%date:/=%.js
--- pack/webpack.config.js
+++ pack/webpack.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   mode: 'production',
-  entry: __dirname + "/src/cassava_ui.js",
+  entry: __dirname + "/src/cassava_menu.js",
   output: {
     path: __dirname +'/out/',
     filename: 'cassava_min.js'
